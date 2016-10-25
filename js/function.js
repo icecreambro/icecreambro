e9b86895d5320585bcead81b74557987e5576e62
Reid Gahan <rrgahan@gmail.com>
2016-10-25 10:15:15 -0500

    Promo code

--- js/function.js
+++ js/function.js
@@ -8,7 +8,7 @@ $('input[type=text]').change(validateForm);
 function validateForm(){var a=$("#name").val(),b=$("#number").val(),c=$("#location").val(),d=$("#time").val(),e=$(":checkbox:checked").first().val();""!=a&&""!=b&&""!=c&&""!=d&&"on"==e?$(".submit-buttons button").removeClass("disabled"):$(".submit-buttons button").addClass("disabled")}
 
 // Validate promo code
-function validateCode(elem){var a=["HILLMAN25","JBUCKS25"],b=$("#mobile-promo-input").val().toUpperCase(),c=$.inArray(b,a)>-1;c?$(this).removeClass().addClass("btn btn-success").html("Success!!"):$(this).removeClass().addClass("btn btn-danger").html("Invalid Code.")}
+function validateCode(elem) {var a = ["HILLMAN25", "JBUCKS25"],b = $("#mobile-promo-input").val().toUpperCase(),c = $.inArray(b, a) > -1;c ? $(elem).removeClass().addClass("btn btn-success btn-lg").html("Success!!"):$(elem).removeClass().addClass("btn btn-danger btn-lg").html("Invalid Code.")}
 
 // Generate random order id
 $(function(){var a=Math.floor(1e3+9e3*Math.random());$('input[name="_subject"]').val("Order ID: "+a)});
