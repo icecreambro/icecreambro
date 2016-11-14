c0ade245b9214298430918ba1236de975f8fcd8e
Reid Gahan <rrgahan@gmail.com>
2016-11-14 10:44:45 -0600

    Change promo, add how it works

--- js/function.js
+++ js/function.js
@@ -19,7 +19,7 @@ function validateForm() {
 }
 
 function validateCode() {
-    var promos = ['HILLMAN25', 'JBUCKS25', 'THINKTHETA', 'RAYMOND2', 'RAYMOND3']
+    var promos = ['HILLMAN25', 'JBUCKS25', 'THANKSGIVING']
     var input = $('.promo input').val().toUpperCase();
     var valid = $.inArray(input, promos) > -1;
     if (valid) {
