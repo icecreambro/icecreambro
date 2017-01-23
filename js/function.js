8b04dcda3599f940eda35fd521a2cee1506e7ea4
Reid Gahan <rrgahan@gmail.com>
2017-01-23 14:59:34 -0600

    First time through +5 minutes

--- js/function.js
+++ js/function.js
@@ -13,11 +13,33 @@ function validateCode() {
     }
 }
 
-$(function () {
+$(function() {
     var id = Math.floor(1000 + Math.random() * 9000)
     $('input[name="_subject"]').val('Order ID: ' + id);
     $('input[name="_next"]').val('complete.html?id='+id);
-});
+})
+
+$(function() {
+    var coeff = 1000 * 60 * 5;
+    var today = new Date();
+    var rounded = new Date(Math.ceil(today.getTime() / coeff) * coeff);
+    var hour = rounded.getHours();
+    var min = rounded.getMinutes()+5;
+    console.log(min);
+    if (min >= 60) {
+        min = min-60;
+        hour+=1;
+        if (hour == 24) {
+            hour = "00";
+        }
+    }
+    while(min.toString().length < 2) {
+        min = "0" + min.toString();
+    }
+    var time = hour + ":" + min;
+    $('.order [type=time]').val(time);
+})
+
 
 //Initialize Popovers
 
